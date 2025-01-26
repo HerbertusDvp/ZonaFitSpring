2f6f6ab361f8527749a66fa9bc46748b1b2a7fd9
El macho <herbertusDvp@gmail.com>
2025-01-25 20:37:59 -0600

    Feat: Zona fit con SpringBoot

---
+++ src/main/java/HerbertusDvp/zonafit/ZonafitApplication.java
@@ -0,0 +1,13 @@
+package HerbertusDvp.zonafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonafitApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ZonafitApplication.class, args);
+	}
+}
