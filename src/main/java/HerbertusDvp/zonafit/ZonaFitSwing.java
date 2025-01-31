ede69f03ecb9c9abb6fb87516672f70b3ec836fc
herbert <HerbertusDvp@gmail.com>
2025-01-31 14:20:54 -0600

    Feat: Encabezados de la table cliente

---
+++ src/main/java/HerbertusDvp/zonafit/ZonaFitSwing.java
@@ -0,0 +1,36 @@
+package HerbertusDvp.zonafit;
+
+import HerbertusDvp.zonafit.gui.ZonaFitForma;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ZonaFitSwing {
+    public static void main(String[] args) {
+
+        //Modo oscuro
+
+       // FlatDarculaLaf.setup();
+
+
+        // INstanciar la fabrica de spring
+
+        ConfigurableApplicationContext contextoSpring =
+                new SpringApplicationBuilder(ZonaFitSwing.class)
+                        .headless(false)
+                        .web(WebApplicationType.NONE)
+                        .run(args);
+
+        // Crear objeto de swing
+
+        SwingUtilities.invokeLater(()->{
+            ZonaFitForma zonaFitForma = contextoSpring.getBean(ZonaFitForma.class);
+            zonaFitForma.setVisible(true);
+        });
+    }
+}
--- src/main/java/HerbertusDvp/zonafit/gui/ZonaFitForma.java
+++ src/main/java/HerbertusDvp/zonafit/gui/ZonaFitForma.java
@@ -6,14 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 @Component
 public class ZonaFitForma extends JFrame{
     private JPanel pPrincipal;
+    private JPanel pTabla;
+    private JTable tCliente;
     IClienteServicio clienteServicio;
+    private DefaultTableModel tablaModeloCliente;
 
     @Autowired
     public ZonaFitForma(ClienteServicio clienteServicio){
         this.clienteServicio = clienteServicio;
+        iniciarForma();
+    }
+
+    private void iniciarForma(){
+        setContentPane(pPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900, 700);
+        setLocationRelativeTo(null); // centrar ventana
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaModeloCliente = new DefaultTableModel(0, 4);
+        String campos [] = {"Id", "Nombre", "Apellido","Membresia"};
+        this.tablaModeloCliente.setColumnIdentifiers(campos);
+        this.tCliente = new JTable(tablaModeloCliente);
     }
 }
